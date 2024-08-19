a5a868f0644bb3dd35caa5478e067ac16d784226
Father1993 <enjoyhillol@gmail.com>
2024-08-19 14:46:46 +1000

    add constants: slider, map, jwt, corsHeaders, products

---
+++ constants/products.ts
@@ -0,0 +1,35 @@
+import { m } from 'framer-motion'
+
+export const productWithoutSizes = [
+  'umbrella',
+  'pen',
+  'notebook',
+  'promotional-souvenirs',
+  'business-souvenirs',
+]
+
+export const productTypes = [
+  'long-sleeves',
+  'bags',
+  'business-souvenirs',
+  'headdress',
+  'hoodie',
+  'notebook',
+  'pen',
+  'outwear',
+  'promotional-souvenirs',
+  't-shirts',
+  'umbrella',
+]
+
+export const productCategories = ['cloth', 'accessories', 'office', 'souvenirs']
+export const allowedColors = ['purpure', 'yellow', 'orange', 'black', 'white']
+export const allowedSizes = ['s', '1', 'm', 'x1', 'xxl']
+export const allowedCollectionsCategories = ['cloth', 'accessories']
+export const allowedCollections = [
+  'street',
+  'black',
+  'casual',
+  'orange',
+  'line',
+]
