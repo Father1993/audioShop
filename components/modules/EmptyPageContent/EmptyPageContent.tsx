d03fc3d60528177c0f5478dde2d29b6f950b7000
Father1993 <enjoyhillol@gmail.com>
2024-09-10 14:50:44 +1000

    start write EmptyPageContent modules

--- components/modules/EmptyPageContent/EmptyPageContent.tsx
+++ components/modules/EmptyPageContent/EmptyPageContent.tsx
@@ -1,4 +1,9 @@
+import { useLang } from '@/hooks/useLang'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { IEmptyPageContentProps } from '@/types/modules'
+import styles from '@/styles/empty-content/index.module.scss'
+import ContentTitle from './ContentTitle'
+import ContentLinks from './ContentLinks'
 
 const EmptyPageContent = ({
   subtitle,
@@ -6,7 +11,26 @@ const EmptyPageContent = ({
   btnText,
   bgClassName,
 }: IEmptyPageContentProps) => {
-  return <div></div>
+  const { lang, translations } = useLang()
+  const isMedia950 = useMediaQuery(950)
+  const isMedia500 = useMediaQuery(500)
+
+  return (
+    <div className={styles.empty_content}>
+      {isMedia950 && <ContentTitle />}
+      <div className={`${styles.empty_content__bg} ${bgClassName}`} />
+      <div className={styles.empty_content__inner}>
+        <span className={styles.empty_content__word}>
+          {translations[lang].common.empty}
+        </span>
+        {!isMedia950 && <ContentTitle />}
+        <div className={styles.empty_content__subtitle}>{subtitle}</div>
+        <div className={styles.empty_content__description}>{description}</div>
+        {!isMedia500 && <ContentLinks btnText={btnText} />}
+      </div>
+      {isMedia500 && <ContentLinks btnText={btnText} />}
+    </div>
+  )
 }
 
 export default EmptyPageContent
