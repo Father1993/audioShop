6b724ccdbbc5bad217504a3985cee2214852a386
Father1993 <enjoyhillol@gmail.com>
2024-09-24 16:58:54 +1000

    start write new routes for profile page: users=> avatars, delete, edit

---
+++ app/api/users/edit/email/verify-code/route.ts
@@ -0,0 +1,36 @@
+import { ObjectId } from 'mongodb'
+import { NextResponse } from 'next/server'
+import clientPromise from '@/lib/mongodb'
+import { getDbAndReqBody } from '@/lib/utils/api-routes'
+
+export async function POST(req: Request) {
+  try {
+    const { db, reqBody } = await getDbAndReqBody(clientPromise, req)
+
+    const codeData = await db
+      .collection('codes')
+      .findOne({ _id: new ObjectId(reqBody.codeId) })
+
+    if (!codeData) {
+      return NextResponse.json({
+        status: 400,
+        error: { message: 'Неправильный код' },
+      })
+    }
+
+    if (codeData.code === reqBody.code) {
+      return NextResponse.json({
+        status: 200,
+        result: true,
+        newEmail: codeData.newEmail,
+      })
+    }
+
+    return NextResponse.json({
+      status: 400,
+      error: { message: 'Неправильный код' },
+    })
+  } catch (error) {
+    throw new Error((error as Error).message)
+  }
+}
