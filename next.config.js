cac41166aaaec76e23ea58b7243cee6a87439317
Father1993 <enjoyhillol@gmail.com>
2025-01-10 14:10:26 +1000

    add: new host in nextjs config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'http', hostname: 'localhost' },
       { protocol: 'https', hostname: 'magnitola-admin.netlify.app' },
+      { protocol: 'https', hostname: 'magitola.netlify.app' },
     ],
   },
 }
